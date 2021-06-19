f23d04ccf7d752ca3e82467f1746fce834a6a751
Carlos Suyon <carlossuyon98@gmail.com>
2021-06-19 13:00:54 -0300

    v.1.0

---
+++ PPAI_Grupo14/Entidades/Deposito.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAI_Grupo14.Entidades
+{
+
+	public class Deposito
+	{
+		// denominacion puede ser int ?  
+		private string denominacion;
+		private int numero;
+		private SectorDeposito sectorDeposito;
+
+		public Deposito()
+		{
+
+		}
+
+		public Deposito(string denominacion, int numero)
+		{
+			this.denominacion = denominacion;
+			this.numero = numero;
+		}
+
+		public string Denominacion
+		{
+			get => denominacion;
+			set => denominacion = value;
+		}
+		public int Numero
+		{
+			get => numero;
+			set => numero = value;
+		}
+	}
+
+}
---
+++ PPAI_Grupo14/Entidades/TipoExposicion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAI_Grupo14.Entidades
+{
+	//clase Tipo Exposicion 
+	public class TipoExposicion
+	{
+		private string descripcion;
+		private string nombre;
+
+		public TipoExposicion()
+		{
+
+		}
+		public TipoExposicion(string descripcion, string nombre)
+		{
+			this.descripcion = descripcion;
+			this.nombre = nombre;
+		}
+		public string Nombre
+		{
+			get => nombre;
+			set => nombre = value;
+		}
+
+		public string Descripcion
+		{
+			get => descripcion;
+			set => descripcion = value;
+		}
+
+	}
+
+}
